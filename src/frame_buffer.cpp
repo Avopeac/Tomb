3e324e9f3c66f74e0dcf5a097299b52536240c16
Andreas Larsson <avopeac@gmail.com>
2017-07-05 07:45:27 +0200

    Downgraded the framebuffer creation to support a lesser version of OpenGL.

--- src/frame_buffer.cpp
+++ src/frame_buffer.cpp
@@ -10,50 +10,60 @@ FrameBuffer::FrameBuffer(Uint32 width, Uint32 height, bool depth, bool dynamic_r
 	old_viewport_width_(0), old_viewport_height_(0)
 {
 
-	glCreateFramebuffers(1, &id_);
-
 	// Create color attachment
+	GLuint color_attachment = 0;
 	{
 		GLenum internalFormat = dynamic_range_enabled ? GL_RGBA16F : GL_RGBA8;
-		GLenum format = dynamic_range_enabled ? GL_FLOAT : GL_UNSIGNED_BYTE;
+		GLenum format = GL_RGBA;
+		GLenum type = dynamic_range_enabled ? GL_FLOAT : GL_UNSIGNED_BYTE;
 
-		GLuint color_attachment;
-		glCreateTextures(GL_TEXTURE_2D, 1, &color_attachment);
-		glBindTextureUnit(0, color_attachment);
+		glGenTextures(1, &color_attachment);
+		glBindTexture(GL_TEXTURE_2D, color_attachment);
 		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width_, height_, 0,
-			GL_RGBA, format, nullptr);
-		glTextureParameteri(color_attachment, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTextureParameteri(color_attachment, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTextureParameteri(color_attachment, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTextureParameteri(color_attachment, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			format, type, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glBindTexture(GL_TEXTURE_2D, 0);
+
 		textures_.push_back(color_attachment);
 		texture_bind_points_.push_back(-1);
-
-		// Attach to framebuffer
-		glNamedFramebufferTexture(id_, GL_COLOR_ATTACHMENT0, color_attachment, 0);
 	}
 	
 	// Create depth attachment
+	GLuint depth_attachment = 0;
 	if (depth)
 	{
-		GLuint depth_attachment;
-		glCreateTextures(GL_TEXTURE_2D, 1, &depth_attachment);
-		glBindTextureUnit(0, depth_attachment);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, width_, height_, 0,
-			GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, nullptr);
-		glTextureParameteri(depth_attachment, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTextureParameteri(depth_attachment, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTextureParameteri(depth_attachment, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTextureParameteri(depth_attachment, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		
+		GLenum internalFormat = GL_DEPTH_COMPONENT16;
+		GLenum format = GL_DEPTH_COMPONENT;
+		GLenum type = GL_UNSIGNED_INT;
+
+		glGenTextures(1, &depth_attachment);
+		glBindTexture(GL_TEXTURE_2D, depth_attachment);
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width_, height_, 0,
+			format, type, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glBindTexture(GL_TEXTURE_2D, 0);
+
 		depth_texture_ = depth_attachment;
 		depth_texture_bind_point_ = -1;
-
-		// Attach to framebuffer
-		glNamedFramebufferTexture(id_, GL_DEPTH_ATTACHMENT, depth_texture_, 0);
 	}
 
-	GLenum status = glCheckNamedFramebufferStatus(id_, GL_FRAMEBUFFER);
+	glGenFramebuffers(1, &id_);
+	glBindFramebuffer(GL_FRAMEBUFFER, id_);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color_attachment, 0);
+	if (depth)
+	{
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_attachment, 0);
+	}
+
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 	switch (status)
 	{
 	case GL_FRAMEBUFFER_UNDEFINED: { debug::Log(SDL_LOG_PRIORITY_ERROR, SDL_LOG_CATEGORY_RENDER, "Framebuffer undefined."); } break;
@@ -76,7 +86,8 @@ void FrameBuffer::BindColorAttachmentTexture(size_t index, GLuint unit)
 {
 	if (index < textures_.size())
 	{
-		glBindTextureUnit(unit, textures_[index]);
+		glActiveTexture(GL_TEXTURE0 + unit);
+		glBindTexture(GL_TEXTURE_2D, textures_[index]);
 		texture_bind_points_[index] = unit;
 	}
 }
@@ -85,7 +96,8 @@ void FrameBuffer::UnbindColorAttachmentTexture(size_t index)
 {
 	if (index < textures_.size())
 	{
-		glBindTextureUnit(texture_bind_points_[index], textures_[index]);
+		glActiveTexture(GL_TEXTURE0 + texture_bind_points_[index]);
+		glBindTexture(GL_TEXTURE_2D, 0);
 		texture_bind_points_[index] = -1;
 	}
 }
@@ -94,7 +106,8 @@ void FrameBuffer::BindDepthAttachmentTexture(GLuint unit)
 {
 	if (depth_texture_ != 0)
 	{
-		glBindTextureUnit(unit, depth_texture_);
+		glActiveTexture(GL_TEXTURE0 + unit);
+		glBindTexture(GL_TEXTURE_2D, unit);
 		depth_texture_bind_point_ = unit;
 	}
 }
@@ -103,7 +116,8 @@ void FrameBuffer::UnbindDepthAttachmentTexture()
 {
 	if (depth_texture_ != 0)
 	{
-		glBindTextureUnit(depth_texture_bind_point_, 0);
+		glActiveTexture(GL_TEXTURE0 + depth_texture_bind_point_);
+		glBindTexture(GL_TEXTURE_2D, 0);
 		depth_texture_bind_point_ = -1;
 	}
 }
@@ -111,9 +125,13 @@ void FrameBuffer::UnbindDepthAttachmentTexture()
 void FrameBuffer::Blit(Sint32 sx0, Sint32 sx1, Sint32 sy0, Sint32 sy1, 
 	Sint32 dx0, Sint32 dx1, Sint32  dy0, Sint32 dy1)
 {
-	glNamedFramebufferReadBuffer(id_, GL_COLOR_ATTACHMENT0);
-	glBlitNamedFramebuffer(id_, 0, sx0, sy0, sx1, sy1, dx0, dy0, dx1, dy1,
-		GL_COLOR_BUFFER_BIT, GL_NEAREST);
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, id_);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	
+	glReadBuffer(GL_COLOR_ATTACHMENT0);
+	glDrawBuffer(GL_BACK);
+
+	glBlitFramebuffer(sx0, sy0, sx1, sy1, dx0, dy0, dx1, dy1, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 }
 
 void FrameBuffer::BindDraw()
@@ -140,7 +158,7 @@ void FrameBuffer::UnbindDraw()
 		//glNamedFramebufferDrawBuffer(0, GL_BACK);
 		//glNamedFramebufferReadBuffer(id_, GL_COLOR_ATTACHMENT0);
 		//glNamedFramebufferReadBuffer(id_, GL_DEPTH_ATTACHMENT);
-		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, id_);
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 	}
 }
 
